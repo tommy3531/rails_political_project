bbe820abd885eb4f04ca25e74f80b28dec73fc3b
Tom Marler <thomas.marler@hotmail.com>
2018-05-14 16:12:56 -0500

    Added hello action in the application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-	root 'pages#home'
+	# root 'pages#home'
+	root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
