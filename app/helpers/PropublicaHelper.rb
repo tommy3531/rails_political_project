230607a19170238ed7472ccb0b2484ae662635f8
Tom Marler <thomas.marler@hotmail.com>
2018-05-22 11:26:22 -0500

    Deleted helper

--- app/helpers/PropublicaHelper.rb
+++
@@ -1,9 +0,0 @@
-module PropublicaHelper
-	client = ProPublica.new('SpzjlPZlkMlPKKGCLQS1OqZtCN96lPl7sszOTKra')
-
-
-	def get_member
-	  	client = ProPublica.new('SpzjlPZlkMlPKKGCLQS1OqZtCN96lPl7sszOTKra')
-	  	@a = client.get_member("A000374")
-	end
-end
